1d1dafd8ce696900dc6a268f2ee6b426ea8564fd
luan_sampaio <luansampaio0294@gmail.com>
2025-10-08 09:58:35 -0300

    Lógica das operações e mensagens

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,53 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        imprimirMenu();
+        imprimirInicial();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean executando = true;
+
+        while (executando) {
+            imprimirMenu();
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Implementar");
+                    break;
+                case 2:
+                    System.out.println("Implementar");
+                    break;
+                case 3:
+                    System.out.println("Implementar");
+                    break;
+                case 4:
+                    System.out.println("Implementar");
+                    break;
+                case 5:
+                    System.out.println("Implementar");
+                    break;
+                case 6:
+                    System.out.println("Implementar");
+                    break;
+                case 7:
+                    imprimirSaida();
+                    executando = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida! Escolha novamente.");
+            }
+        }
+        scanner.close();
     }
 
-    public static void imprimirMenu() {
+    public static void imprimirInicial() {
         System.out.println("===========================================");
         System.out.println("    Bem vindo ao Banco L.V Sampaio");
         System.out.println("===========================================");
+    }
+
+    public static void imprimirMenu() {
         System.out.println();
         System.out.println("Operações disponíveis:");
         System.out.println("1 - Criar Conta; ");
@@ -18,5 +59,12 @@ public class Main {
         System.out.println("7 - Sair;");
         System.out.println();
         System.out.print("Digite o numero da operação desejada: ");
+    }
+
+    public static void imprimirSaida() {
+        System.out.println("\n========================================");
+        System.out.println("Operação finalizada!");
+        System.out.println("Volte sempre!");
+        System.out.println("========================================\n");
     }
 }
