31990c9c11226d81a8a515b7128df769ee3533bf
luan_sampaio <luansampaio0294@gmail.com>
2025-10-08 13:43:01 -0300

    feat: ContaCorrente e ContaPoupanca

---
+++ src/ContaCorrente.java
@@ -0,0 +1,19 @@
+public class ContaCorrente extends Conta{
+    public ContaCorrente(int numero, String nome) {
+        super(numero, nome);
+    }
+    @Override
+    public void depositar() {
+
+    }
+
+    @Override
+    public void sacar() {
+
+    }
+
+    @Override
+    public void transferir() {
+
+    }
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,20 @@
+public class ContaPoupanca extends Conta{
+    public ContaPoupanca(int numero, String cliente) {
+        super(numero, cliente);
+    }
+
+    @Override
+    public void depositar() {
+
+    }
+
+    @Override
+    public void sacar() {
+
+    }
+
+    @Override
+    public void transferir() {
+
+    }
+}
